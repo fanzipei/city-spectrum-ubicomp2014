0a049a043a7195ba3628eb1df471e219f4a2f30d
fan <fanzipei@iis.u-tokyo.ac.jp>
2017-07-27 20:39:56 +0900

    ntf revised

--- ntf.py
+++ ntf.py
@@ -5,75 +5,86 @@ import numpy as np
 from numpy.linalg import inv
 from numpy.linalg import norm
 
-def read_file( filename ):
+# Constant Definition
+K = 10
+LAMBDA = 0.5
+MAXITER = 5
+num_time = 48
 
-    with open( filename , 'r' ) as f:
-        line = f.readline()
-        num_locations, num_time, num_days = [ int(n) for n in line.split() ]
-        tensor_list = np.zeros( [num_locations,num_time,num_days] )
 
-        # Read tensor from file
-        for i in range(num_locations):
+def read_file(filename):
 
-            # Read the coordinates
-            line = f.readline()
-            dis_lat, dis_lon = [ int(n) for n in line.split() ]
+    # with open( filename , 'r' ) as f:
+        # line = f.readline()
+        # num_locations, num_time, num_days = [ int(n) for n in line.split() ]
+        # tensor_list = np.zeros( [num_locations,num_time,num_days] )
 
-            # Read the POI vector
-            line = f.readline()
+        # # Read tensor from file
+        # for i in range(num_locations):
 
-            # Read the people flow matrix of each location
-            for j in range(num_time):
-                line = f.readline()
-                tensor_list[i][j] = [ float(n) for n in line.split() ]
+            # # Read the coordinates
+            # line = f.readline()
+            # dis_lat, dis_lon = [ int(n) for n in line.split() ]
 
-    return num_locations, num_time, num_days, tensor_list
+            # # Read the POI vector
+            # line = f.readline()
 
-def khatrirao( a , b ):
+            # # Read the people flow matrix of each location
+            # for j in range(num_time):
+                # line = f.readline()
+                # tensor_list[i][j] = [ float(n) for n in line.split() ]
+    tensor = np.genfromtxt(filename, dtype=np.float, delimiter=',')
+    num_locations = tensor.shape[0]
+    num_days = tensor.shape[1] / num_time
+    tensor = tensor.reshapea([-1, num_time, num_days])
+
+    return num_locations, num_time, num_days, tensor
+
+def khatrirao(a, b):
     assert a.shape[1] == b.shape[1]
     return np.array( [np.outer(col_a,col_b).flatten().T for col_a, col_b in zip( a.T , b.T )] ).T
 
-def positive_constrain( mat ):
-    for x in np.nditer( mat , op_flags=['readwrite']):
-        x[...] = max( x , 0.000000000000001 )
 
-def cal_rec_error( y1 , u , v , w ):
-    uvw = u.dot( khatrirao( w , v ).T )
-    return norm( y1 - uvw )
+def cal_rec_error(y1, u, v, w):
+    uvw = u.dot(khatrirao(w, v).T)
+    return norm(y1 - uvw)
 
-def cal_rec_error_loc( y1 , u , v , w ):
-    uvw = u.dot( khatrirao( w , v ).T )
-    return norm( y1 - uvw , axis=1 )
 
-def cal_rec_error_time( y2 , u , v , w ):
-    uvw = v.dot( khatrirao( w , u ).T )
-    return norm( y2 - uvw , axis=1 )
+def cal_rec_error_loc(y1, u, v, w):
+    uvw = u.dot(khatrirao(w, v).T)
+    return norm(y1 - uvw, axis=1)
 
-def cal_rec_error_day( y3 , u , v , w ):
-    uvw = w.dot( khatrirao( v , u ).T )
-    return norm( y3 - uvw , axis=1 )
 
-def ntf( tensor , num_locations , num_time , num_days , K , LAMBDA , MAXITER ):
+def cal_rec_error_time(y2, u, v, w):
+    uvw = v.dot( khatrirao(w, u).T )
+    return norm(y2 - uvw, axis=1)
+
+
+def cal_rec_error_day(y3, u, v, w):
+    uvw = w.dot(khatrirao(v, u).T)
+    return norm(y3 - uvw, axis=1)
+
+
+def ntf(tensor, num_locations, num_time, num_days, K, LAMBDA, MAXITER):
     # Normalize the tensor
-    norm_t = tensor.sum( axis = 0 ) * 0.01
+    norm_t = tensor.sum(axis=0)
     for i in range(num_time):
         for j in range(num_days):
-            tensor[:,i,j] /= norm_t[i][j]
-            # tensor[:,i,j] /= 100.0
+            tensor[:, i, j] /= norm_t[i][j]
 
     # Unfolding
-    y_1 = np.concatenate( [tensor[:,:,i] for i in range( num_days )] , axis = 1 )
-    y_2 = np.concatenate( [tensor[:,:,i].T for i in range( num_days )] , axis = 1 )
-    y_3 = np.concatenate( [tensor[:,i,:].T for i in range( num_time )] , axis = 1 )
+    y_1 = np.concatenate([tensor[:, :, i] for i in range(num_days)], axis=1)
+    y_2 = np.concatenate([tensor[:, :, i].T for i in range( num_days )], axis=1)
+    y_3 = np.concatenate([tensor[:, i, :].T for i in range( num_time )], axis=1)
 
     # Initialize the best configuration
-    u_best = np.zeros( [num_locations,K] )
-    v_best = np.zeros( [num_time,K] )
-    w_best = np.zeros( [num_days,K] )
+    u_best = np.zeros([num_locations, K])
+    v_best = np.zeros([num_time, K])
+    w_best = np.zeros([num_days, K])
     least_error = np.inf
 
     # N trial of Factorization
-    for i in range( 10 ):
+    for i in range(10):
 
         # Factorization initialization
         u = np.random.ranf( [num_locations, K] )
@@ -83,15 +94,15 @@ def ntf( tensor , num_locations , num_time , num_days , K , LAMBDA , MAXITER ):
         # Factorization iteration
         for j in range(MAXITER):
             print 'Iteration %d' % (j+1)
-            # u = y_1.dot( khatrirao( w , v ) ).dot( inv( w.T.dot( w ) * v.T.dot( v ) + LAMBDA * np.identity(K) ) )
-            u = ( y_1.dot( khatrirao( w , v ) ) - LAMBDA ).dot( inv( w.T.dot( w ) * v.T.dot( v ) ) )
-            positive_constrain( u )
-            # v = y_2.dot( khatrirao( w , u ) ).dot( inv( w.T.dot( w ) * u.T.dot( u ) + LAMBDA * np.identity(K) ) )
-            v = ( y_2.dot( khatrirao( w , u ) ) - LAMBDA ).dot( inv( w.T.dot( w ) * u.T.dot( u ) ) )
-            positive_constrain( v )
-            # w = y_3.dot( khatrirao( v , u ) ).dot( inv( v.T.dot( v ) * u.T.dot( u ) + LAMBDA * np.identity(K) ) )
-            w = ( y_3.dot( khatrirao( v , u ) ) - LAMBDA ).dot( inv( v.T.dot( v ) * u.T.dot( u ) ) )
-            positive_constrain( w )
+            u = y_1.dot( khatrirao( w , v ) ).dot( inv( w.T.dot( w ) * v.T.dot( v ) + LAMBDA * np.identity(K) ) )
+            # u = ( y_1.dot( khatrirao( w , v ) ) - LAMBDA ).dot( inv( w.T.dot( w ) * v.T.dot( v ) ) )
+            u[u < 0] = 1e-10
+            v = y_2.dot( khatrirao( w , u ) ).dot( inv( w.T.dot( w ) * u.T.dot( u ) + LAMBDA * np.identity(K) ) )
+            v[v < 0] = 1e-10
+            # v = ( y_2.dot( khatrirao( w , u ) ) - LAMBDA ).dot( inv( w.T.dot( w ) * u.T.dot( u ) ) )
+            w = y_3.dot( khatrirao( v , u ) ).dot( inv( v.T.dot( v ) * u.T.dot( u ) + LAMBDA * np.identity(K) ) )
+            w[w < 0] = 1e-10
+            # w = ( y_3.dot( khatrirao( v , u ) ) - LAMBDA ).dot( inv( v.T.dot( v ) * u.T.dot( u ) ) )
 
         # Best configuration
         cur_error = cal_rec_error( y_1 , u, v, w )
@@ -110,13 +121,8 @@ def ntf( tensor , num_locations , num_time , num_days , K , LAMBDA , MAXITER ):
 
 def main():
 
-    # Constant Definition
-    K = 10
-    LAMBDA = 0.1
-    MAXITER = 5
-
     # Read file
-    num_locations, num_time, num_days, tensor = read_file( '/home/fan/work/data/tensor_tokyo.txt' )
+    num_locations, num_time, num_days, tensor = read_file('./fukushima_coastal_pd.csv')
 
     # Decomposition
     (u, v, w, eu, ev, ew) = ntf( tensor , num_locations , num_time , num_days , K , LAMBDA , MAXITER )
